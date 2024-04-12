1ec3554917bf215d51ca3bfc2a86006891205b02
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2024-04-13 01:42:00 +0200

    prefetch link

---
+++ app/(site)/_lightgallery/page.tsx
@@ -0,0 +1,14 @@
+
+import { LightGalleryMasonry } from "@/app/unused/lightGalleryMasonry";
+import { getProjectsData } from "@/sanity/lib/queryLoaders";
+
+export default async function Page() {
+  const projects = await getProjectsData();
+
+  return (
+    <>
+   
+      <LightGalleryMasonry projects={projects}/>
+    </>
+  );
+}
