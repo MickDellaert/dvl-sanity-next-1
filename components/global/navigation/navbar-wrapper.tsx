f6483dfd44289396ed3c01ee8e166ca5e1380949
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2025-03-16 22:37:08 +0100

    working on nav

--- components/global/navigation/navbar-wrapper.tsx
+++ components/global/navigation/navbar-wrapper.tsx
@@ -1,15 +1,9 @@
 import { getSettings } from "@/sanity/lib/queryLoaders";
 import NavbarContainer from "./navbar-container";
-import Hamburger from "./hamburger";
 
 export default async function NavbarWrapper() {
   const settingsData = await getSettings();
   const menuItems = settingsData.menuItems;
 
-  return (
-    // <nav className="fixed top-0 z-50 h-20 w-full bg-white">
-    // <nav className="fixed left-0 top-0 z-50 h-20 w-full bg-green-200 px-8 pt-4 md:pt-8">
-    <NavbarContainer menuItems={menuItems} />
-    // </nav>
-  );
+  return <NavbarContainer menuItems={menuItems} />;
 }
--- components/global/navigation/navlinks-mobile.tsx
+++ components/global/navigation/navlinks-mobile.tsx
@@ -2,103 +2,60 @@
 
 import { MenuItem } from "@/sanity/types";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { resolveHref } from "@/sanity/lib/utils";
-import { useEffect, useState } from "react";
-import Hamburger from "./hamburger";
-import HamburgerButton from "./hamburgerButton";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 type NavProps = {
   menuItems: MenuItem[];
+  pathname: string;
+  openMenu: boolean;
+  onRouteCheckAction: (href: string) => void;
 };
 
-export default function NavLinksMobile({ menuItems }: NavProps) {
-  const [hasMounted, setHasMounted] = useState(false);
-
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-  const [openMenu, setOpenMenu] = useState(false);
-  const pathname = usePathname();
-
-  const toggleMenu = () => {
-    setOpenMenu(!openMenu);
-  };
-
-  const onRoute = (href: string) => {
-    if (pathname === href) {
-      setOpenMenu(false);
-    }
-  };
-
-  useEffect(() => {
-    setOpenMenu(false);
-  }, [pathname]);
-
-  if (!hasMounted) return null;
-
+export default function NavLinksMobile({
+  menuItems,
+  openMenu,
+  pathname,
+  onRouteCheckAction,
+}: NavProps) {
   return (
-    <div className="">
-      {/* {!openMenu && (
-        // <button className="relative top-1 z-50 w-8" onClick={toggleMenu}>
-        //   <Hamburger />
-        // </button>
-        <HamburgerButton onClick={toggleMenu} />
-      )}
+    <AnimatePresence>
       {openMenu && (
-        // <button
-        //   className="relative z-50 h-8 w-8 pb-1 text-3xl leading-[8px]"
-        //   onClick={toggleMenu}
-        // >
-        //   x
-        // </button>
-        <HamburgerButton onClick={toggleMenu} />
-      )} */}
-      <HamburgerButton onClick={toggleMenu} openMenu={openMenu} />
-
-      <motion.div
-        className={`fixed left-0 top-0 z-40 flex h-dvh w-full flex-col bg-stone-200 px-6 pb-64 md:pb-64`}
-        // style={
-        //   openMenu
-        //     ? { display: "flex", position: "fixed", top: 0, left: 0 }
-        //     : { display: "none", position: "static" }
-        // }
-        animate={openMenu ? "open" : "closed"}
-        variants={{ open: { opacity: 100 }, closed: { opacity: 0 } }}
-        initial={false as unknown as undefined}
-      >
-        <div
-          className="relative mt-40 flex flex-col gap-y-3 pt-8 text-3xl
-          before:absolute before:left-0 before:top-0 before:h-[4px] before:w-16 before:bg-black before:content-['']"
+        <motion.div
+          className={`fixed left-0 top-0 z-40 flex h-dvh w-full flex-col bg-stone-200 px-6 pb-64 md:pb-64`}
+          // animate={openMenu ? "open" : "closed"}
+          // variants={{ open: { opacity: 100 }, closed: { opacity: 0 } }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 100 }}
+          exit={{ opacity: 0 }}
         >
-          {menuItems.map((setting) => {
-            const href = resolveHref(setting._type, setting.slug);
+          <div
+            className="relative mt-40 flex flex-col gap-y-3 pt-8 text-3xl
+        before:absolute before:left-0 before:top-0 before:h-[4px] before:w-16 before:bg-black before:content-['']"
+          >
+            {menuItems.map((setting) => {
+              const href = resolveHref(setting._type, setting.slug);
 
-            // console.log(href);
+              if (!href) {
+                return null;
+              }
 
-            if (!href) {
-              return null;
-            }
-
-            return (
-              <Link
-                //   className={`${pathname === href || pathname.includes(href) ? "bg-white underline decoration-2 underline-offset-8 mix-blend-difference invert" : ""}
-                // decoration-2 underline-offset-8 hover:bg-white hover:underline hover:mix-blend-difference hover:invert`}
-
-                className={`${pathname === href || pathname.includes(href) ? "text-gray-500" : ""} 
-              hover:text-black`}
-                key={setting.title}
-                href={href}
-                onClick={() => onRoute(href)}
-                prefetch={true}
-              >
-                {setting.title}
-              </Link>
-            );
-          })}
-        </div>
-      </motion.div>
-    </div>
+              return (
+                <Link
+                  className={`${pathname === href || pathname.includes(href) ? "text-gray-500" : ""} 
+            hover:text-black`}
+                  key={setting.title}
+                  href={href}
+                  onClick={() => onRouteCheckAction(href)}
+                  prefetch={true}
+                >
+                  {setting.title}
+                </Link>
+              );
+            })}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
