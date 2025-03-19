5686a21bf817efa31aa5fca4a4b04a3b3b95212d
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2025-03-19 18:29:33 +0100

    disable nav blend mode

--- components/global/navigation/navbar-container.tsx
+++ components/global/navigation/navbar-container.tsx
@@ -2,6 +2,8 @@ import NavLinks from "./navbar-links";
 import { MenuItem } from "@/sanity/types";
 import Logo from "./navbar-logo";
 import NavMobile from "./navbar-mobile";
+import NavbarLinksMobile from "./navbar-links-mobile";
+import NavbarHamburger from "./navbar-hamburger";
 
 type NavProps = {
   menuItems: MenuItem[];
@@ -9,14 +11,16 @@ type NavProps = {
 
 export default function NavbarContainer({ menuItems }: NavProps) {
   return (
-    <div className="items-top fixed left-0 top-0 z-50 flex h-20 w-full flex-row justify-between px-x pt-6 mix-blend-difference invert md:px-8 md:pt-8">
-      <Logo />
-      <div className="hidden lg:block">
-        <NavLinks menuItems={menuItems} />
+    <>
+      <div className="items-top fixed left-0 top-0 z-50 flex h-20 w-full flex-row justify-between px-x pt-6 md:px-8 md:pt-8">
+        <Logo />
+        <div className="hidden lg:block">
+          <NavLinks menuItems={menuItems} />
+        </div>
+        <div className="block lg:hidden">
+          <NavMobile menuItems={menuItems} />
+        </div>
       </div>
-      <div className="block lg:hidden ">
-        <NavMobile menuItems={menuItems} />
-      </div>
-    </div>
+    </>
   );
 }
